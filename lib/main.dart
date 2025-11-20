5fd6d811c0dd29d6e9638dc16487bbaf7751abf3
Sid <sidven5000@gmail.com>
2025-11-21 08:54:47 +1300

    updated to version 1.1.15, mainly added the quick guide in the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ import 'providers/manage_meetings_provider.dart';
 import 'providers/view_meeting_provider.dart';
 
 // Cache busting version - increment this when making changes that require browser cache clearing
-const String appVersion = '1.1.14';
+const String appVersion = '1.1.15';
 
 // Custom page transitions builder that removes all animations
 class NoTransitionsBuilder extends PageTransitionsBuilder {
